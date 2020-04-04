3156b099328a4e406973635fdfa4e020e931116d
Well <well@edu.unifor.br>
2020-04-04 19:51:17 -0300

    Add hamburguer button function and function modal button contact

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ function onOff() {
         }
     })
 }
+
+// Reponsive Menu
+let show = true
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+const menuButton = menuSection.querySelector(".contact-header")
+
+menuToggle.addEventListener("click", () => {
+    
+    document.body.style.overflow = show ? "hidden" : "initial"
+    menuSection.classList.toggle("on", show)
+
+    // Function when menu hamburger shows up. Click contact button, and menu hamburger close and contact box comes up.
+    menuButton.addEventListener("click", () => {
+        menuSection.classList.toggle("on", show)
+        document.body.style.overflow = show ? "hidden" : "initial"
+    })
+
+    
+    show = !show
+})
